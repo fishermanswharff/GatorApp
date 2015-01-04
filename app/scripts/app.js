13b2b8337c8c3ab370b1a0b1f10fbf696ae8377b
fishermanswharff <fishermanswharff@github.com>
2015-01-04 18:24:59 -0500

    Autopush Gator2 App Master Branch

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,19 +17,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    'ngTouch',
+    'MainController',
+    'MainDirective'
+  ]);
+  
