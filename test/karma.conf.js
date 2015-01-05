343fbe34cc202c888c3dc059fd77c36921f5d40c
fishermanswharff <fishermanswharff@github.com>
2015-01-05 02:23:01 -0500

    Autopush Gator2 App Master Branch

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'app/scripts/controllers/MainController.js',
+      'app/scripts/directives/MainDirective.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
