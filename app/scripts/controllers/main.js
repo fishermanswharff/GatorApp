2c879f6dbb45dcb7ece90ff9fa713d09373ddd5a
fishermanswharff <fishermanswharff@github.com>
2015-04-05 15:21:30 -0400

    Autopush Gator2 App Master Branch

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,20 +9,29 @@
  */
 angular.module('MainController')
   .controller('MainCtrl', ['$scope','$http','$window','trace','AuthFactory','ServerUrl',function($scope,$http,$window,trace,AuthFactory,ServerUrl) {
-    
     $scope.tokenParams = '';
-
     $scope.signIntoTwitter = function(){
-      $http.get(ServerUrl + 'users/auth/twitter').success(function(response){
+      $http.get(ServerUrl + 'users/auth/twitter').success(function(data, status, headers, config){
         $scope.userToken = $window.sessionStorage.getItem('gator-token');
-        $scope.tokenParams = response.token + '&' + response.secret;
-        $scope.authenticateTwitter($scope.tokenParams);
+        trace(data, status, headers, config);
+        if(status == 202){
+          $scope.tokenParams = response.token + '&' + response.secret;
+          $scope.authenticateTwitter($scope.tokenParams);
+        } else if(status == 203) {
+          // this comes back if the user already has their twitter account stored in the api (already completed the oauth flow)
+          // save the data that comes back (message and username), render somewhere on screen
+        };
+      }).error(function(data, status, headers, config){
+        trace(data, status, headers, config);
       });
     };
 
     $scope.authenticateTwitter = function(params){
       trace(params,'authenticating twitter');
       $window.location.href = 'https://api.twitter.com/oauth/authenticate?'+params;
+      // $http.get('https://api.twitter.com/oauth/authenticate?'+params).then(function(response){
+      //   trace('response from authenticating with twitter: ',response);
+      // });
     };
 
     $scope.isLoggedIn = function(){
